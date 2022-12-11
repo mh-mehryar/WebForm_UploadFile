ee3c697a7be161910842918163cabbaf6540a606
Mohammad Hossein <mh.mehryar@gmail.com>
2022-12-11 22:38:58 +0330

    Add project files.

---
+++ WebApplication5/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebForm_UploadFile.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="UpFile" runat="server"></asp:FileUpload>
+            <br />
+
+            <asp:Button ID="BtnUp" Text="Upload" runat="server" OnClick="UpBtn_Click"></asp:Button>
+            <br />
+
+            <asp:Label ID="LabMsg" runat="server"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication5/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForm_UploadFile
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UpBtn_Click(object sender, EventArgs e)
+        {
+            LabMsg.Text = string.Empty;
+            if (!UpFile.HasFile)
+            {
+                LabMsg.Text = "لطفا فایل عکس خود را انتخاب نمائید";
+                return;
+            }
+            string[] ValidExtensions = { ".jpg", ".png", ".bmp",".gif" };
+            string FileExtension = System.IO.Path.GetExtension(UpFile.FileName);
+            if (ValidExtensions.Contains(FileExtension))
+            {
+                double FileSize = (UpFile.PostedFile.ContentLength) / 1024.0;
+                if (FileSize > 500 )
+                {
+                    LabMsg.Text = "اندازه فایل عکس شما بیشتر از 500 کیلوبایت می باشد";
+                    return;
+                }
+                string StrServerPath = Server.MapPath("");
+                StrServerPath += "/UserFiles/Images/" + UpFile.FileName;
+                UpFile.SaveAs(StrServerPath);
+                
+            }
+            else
+            {
+                LabMsg.Text = "لطفا نوع فایل خود را درست انتخاب نمائید";
+                return;
+            }
+        }
+    }
+}
